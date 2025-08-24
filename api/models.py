70a7082d6843d0296a86e2900023f567370f2e9a
kawsarmatubbor <kawsarmatubbordec@gmail.com>
2025-08-24 18:00:32 +0600

    Custom user model ok

---
+++ api/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    BLOOD_GROUP_CHOICES = (
+        ('A+', 'A+'),
+        ('A-', 'A-'),
+        ('B+', 'B+'),
+        ('B-', 'B-'),
+        ('AB+', 'AB+'),
+        ('AB-', 'AB-'),
+        ('O+', 'O+'),
+        ('O-', 'O-'),
+    )
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    )
+    username = None
+    email = models.EmailField(max_length=254, unique=True)
+    description = models.TextField(blank=True, null=True)
+    blood_group = models.CharField(max_length=3, choices=BLOOD_GROUP_CHOICES)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
+    dob = models.DateField(blank=True, null=True)
+    present_address = models.TextField(blank=True, null=True)
+    permanent_address = models.TextField(blank=True, null=True)   
+    phone_number = models.CharField(max_length=100)
+    donor = models.BooleanField(default=False)
+    last_donation_date = models.DateField(blank=True, null=True)
+    is_available = models.BooleanField(default=False)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "blood_group", "phone_number"]
+
+
+    def __str__(self):
+        return f"{self.email} ({self.blood_group})"
